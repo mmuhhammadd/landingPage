a2656c75a0992aee385c1db8a6c4f46d94453f0a
mmuhhammadd <mmuhhammadd@outlook.com>
2021-06-22 14:48:02 +0200

    feat: add navigation items highlighting

--- js/app.js
+++ js/app.js
@@ -22,6 +22,8 @@ const sections = document.getElementsByTagName("section");
 
 // Defining variable to represent nav list
 const navList = document.getElementById("navbar__list");
+
+
 /**
  * End Global Variables
  * Start Helper Functions
@@ -39,37 +41,59 @@ const navList = document.getElementById("navbar__list");
 const DOMFragment = new DocumentFragment();
 for (let i = 0; i < sections.length; i++)
 {
+    // creating list item for every nav item
+    const menuItem = document.createElement("li");
+    menuItem.classList.add(`${sections[i].id}`);
+    
     const menuLink = document.createElement("a");
     menuLink.textContent = sections[i].getAttribute("data-nav");
     menuLink.classList.add("menu__link");
     menuLink.setAttribute('href', `#${sections[i].id}`);
-    DOMFragment.appendChild(menuLink);
+    menuItem.appendChild(menuLink);
+    DOMFragment.appendChild(menuItem);
 }
 navList.appendChild(DOMFragment);
 
 // Add class 'active' to section when near top of viewport
 const setActive = sections => {
 
-    // Getting the current active section
-    const activeNow = document.getElementsByClassName("your-active-class")[0];
-
     // defining variable to hold the id of the active section
     let activeSection;
 
+    // Getting the current active section
+    const activeSectionNow = document.querySelector("section.your-active-class");
+
     for (let i = 0; i < sections.length; i++)
     {
-        if (Math.abs(sections[i].getBoundingClientRect().top)  < Math.abs(activeNow.getBoundingClientRect().top))
+        if (Math.abs(sections[i].getBoundingClientRect().top)  < Math.abs(activeSectionNow.getBoundingClientRect().top))
         {
             activeSection = sections[i].id;
 
             // Getting current active section and deactivating it
-            activeNow.classList.remove("your-active-class");
+            activeSectionNow.classList.remove("your-active-class");
 
             // setting the new active section
             document.getElementById(activeSection).classList.add("your-active-class");
         }
 
     }
+}
+
+// Setting active nav item
+const activeNav = ()=> {
+    // getting current active navigation item
+    const currentlyActiveItem = document.querySelector("li.your-active-class");
+
+    // deactivating current active item if exist
+    if (currentlyActiveItem !== null)
+    {
+        currentlyActiveItem.classList.remove("your-active-class");
+    }
+
+    // setting new active nav item
+    const activeNavItem = document.querySelector(`li.${document.querySelector("section.your-active-class").id}`);
+    activeNavItem.classList.add("your-active-class");
+
 }
 
 // Scroll to anchor ID using scrollTO event
@@ -100,8 +124,9 @@ navList.addEventListener("click", e => {
     })
 })
 
-// Set sections as active
+// Set sections and nav items as active
 setTimeout(window.addEventListener("scroll", ()=> {
     setActive(sections);
+    activeNav();
 }),
 0)
